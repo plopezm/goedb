b9123ce33e3a8b3964027c208323d8649ed860cb
Pablolm <pabloplm@gmail.com>
2017-07-05 23:00:04 +0200

    Minor improvements

--- GoedbDriver.go
+++ GoedbDriver.go
@@ -1,5 +1,23 @@
 package goedb
 
+import "reflect"
+
+type GoedbTable struct{
+	Name    string
+	Columns []GoedbColumn
+	Model  	reflect.Type		`json:"-"`
+}
+
+type GoedbColumn struct{
+	Title   	string
+	Ctype   	string
+	Pk      	bool
+	Unique  	bool
+	Fk      	bool
+	Fkref   	string
+	Autoinc 	bool
+}
+
 type GoedbResult struct {
 	NumRecordsAffected int64
 }
